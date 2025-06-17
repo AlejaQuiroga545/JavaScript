88ec712448643765224f2b7f496859de240bb527
Alejandra Quiroga G. <alejandraquirogag802@gmail.com>
2025-06-16 19:32:35 -0500

    Create d_final.js

---
+++ Semana 1/Taller_js_Aleja_Quiroga/d_final.js
@@ -0,0 +1,17 @@
+// He creado un programa que le da la bienvenida a los usuarios que visitan mi website 🥰
+
+// Primero, creo mis variables
+let nombre;
+let edad;
+let ciudad;
+
+/*
+  Uso prompt() para pedirle a mi usuario sus datos.
+  Lo que mi usuario escriba, se guarda en mis variables.
+*/
+nombre = prompt("¿Cómo te llamas? ☺️");
+edad = prompt("¿Cuántos años tienes? (solo el número) 👀");
+ciudad = prompt("¿En qué ciudad vives? 🤠");
+
+// Mostraré el mensaje de bienvenida a través de la consola
+console.log("Hola " + nombre + ", te damos la bienvenda a AleDesign. Sabemos que tienes " + edad + " años y vives en " + ciudad + ". Por ende, hemos preparado cosas geniales para brindarte la mejor experiencia, disfútalo. 🥰");
